bc3ffe781d8813ae9371106031b1bcf181768d43
jeangab <jeangabriel.gc@gmail.com>
2019-05-03 08:00:05 -0400

    Fix java FirestoreLog which did not compare current logLevel correctly

--- src/android/uk/co/reallysmall/cordova/plugin/firestore/FirestoreLog.java
+++ src/android/uk/co/reallysmall/cordova/plugin/firestore/FirestoreLog.java
@@ -7,32 +7,46 @@ public class FirestoreLog {
   private static int _logLevel = Log.DEBUG;
 
   public static void setLogLevel(String logLevel) {
-    _logLevel = ("debug".equals(logLevel)) ? Log.DEBUG :
-        ("error".equals(logLevel)) ? Log.WARN : Log.ERROR;
+    FirestoreLog.w(FirestorePlugin.TAG, "Setting log level to : " + logLevel);
+    switch(logLevel.toLowerCase()) {
+      case "debug":
+        _logLevel = Log.DEBUG;
+        break;
+      case "error":
+        _logLevel = Log.ERROR;
+        break;
+      case "warn":
+      case "warning":
+        _logLevel = Log.WARN;
+        break;
+      default:
+        FirestoreLog.w(FirestorePlugin.TAG, "New logLevel unknown, leaving previous setting : " + _logLevel);
+        break;
+    }
   }
 
   public static void d(String TAG, String message) {
-    if (_logLevel > Log.DEBUG) {
+    if (_logLevel >= Log.DEBUG) {
       Log.d(TAG, message);
     }
   }
   public static void w(String TAG, String message) {
-    if (_logLevel > Log.WARN) {
+    if (_logLevel >= Log.WARN) {
       Log.w(TAG, message);
     }
   }
   public static void w(String TAG, String message, Exception e) {
-    if (_logLevel > Log.WARN) {
+    if (_logLevel >= Log.WARN) {
       Log.w(TAG, message, e);
     }
   }
   public static void e(String TAG, String message) {
-    if (_logLevel > Log.ERROR) {
+    if (_logLevel >= Log.ERROR) {
       Log.e(TAG, message);
     }
   }
   public static void e(String TAG, String message, Exception e) {
-    if (_logLevel > Log.ERROR) {
+    if (_logLevel >= Log.ERROR) {
       Log.e(TAG, message, e);
     }
   }
